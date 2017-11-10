5be15b5d6d04a696642c9523ce828b5eaa944428
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2017-11-10 18:10:15 +1100

    add standard linting

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 class PromiseQueue {
-  constructor(cb, PromiseFlavour) {
-    this.flushing = false;
-    this.Promise = PromiseFlavour || Promise;
-    this.promise = null;
-    this.queue = [];
-    this.callback = cb;
+  constructor (cb, PromiseFlavour) {
+    this.flushing = false
+    this.Promise = PromiseFlavour || Promise
+    this.promise = null
+    this.queue = []
+    this.callback = cb
   }
 
-  add(fn, opts) {
-    if (typeof fn !== 'function') throw new Error('PromiseQueue.add() expects a function as an argument.');
+  add (fn, opts) {
+    if (typeof fn !== 'function') throw new Error('PromiseQueue.add() expects a function as an argument.')
     return new this.Promise((resolve, reject) => {
-      const attempts = (opts && opts.attempts && opts.attempts > 0) ? opts.attempts : 1;
+      const attempts = (opts && opts.attempts && opts.attempts > 0) ? opts.attempts : 1
       if (this.promise === null) {
-        this.promise = this._wrap(fn, resolve, reject, attempts);
+        this.promise = this._wrap(fn, resolve, reject, attempts)
       } else {
         // shift order based on priority
         const next = {
@@ -20,76 +20,75 @@ class PromiseQueue {
           attempts,
           priority: (opts && opts.priority) ? opts.priority : 0,
           resolve,
-          reject,
-        };
+          reject
+        }
         if (!opts || !opts.priority) {
-          this.queue.push(next);
+          this.queue.push(next)
         } else {
-          let found = false;
+          let found = false
           for (let i = this.length - 1; i >= 0; i--) {
             if (this.queue[i].priority && this.queue[i].priority >= opts.priority) {
-              this.queue.splice(i + 1, 0, next);
-              found = true;
-              break;
+              this.queue.splice(i + 1, 0, next)
+              found = true
+              break
             }
           }
           if (!found) {
-            this.queue.unshift(next);
+            this.queue.unshift(next)
           }
         }
       }
-    });
+    })
   }
 
-  flush() {
-    const concurrent = [this.promise, ...this.queue.map(queued => this._promised(queued.fn).then(queued.resolve, queued.reject))];
-    this.flushing = true;
-    this.queue = [];
+  flush () {
+    const concurrent = [this.promise, ...this.queue.map(queued => this._promised(queued.fn).then(queued.resolve, queued.reject))]
+    this.flushing = true
+    this.queue = []
     const flushed = () => {
-      this.flushing = false;
-      this._next();
+      this.flushing = false
+      this._next()
     }
     return this.Promise.all(concurrent)
-      .then(flushed, flushed);
+      .then(flushed, flushed)
   }
 
-  get length() {
-    return this.queue.length;
+  get length () {
+    return this.queue.length
   }
 
-  _promised(fn) {
+  _promised (fn) {
     try {
-      return this.Promise.resolve(fn());
+      return this.Promise.resolve(fn())
     } catch (e) {
-      return this.Promise.reject(e);
+      return this.Promise.reject(e)
     }
   }
 
-  _next() {
-    if (this.flushing) return;
+  _next () {
+    if (this.flushing) return
     if (this.length > 0) {
-      const nextFn = this.queue.shift();
-      return this._wrap(nextFn.fn, nextFn.resolve, nextFn.reject, nextFn.attempts);
+      const nextFn = this.queue.shift()
+      return this._wrap(nextFn.fn, nextFn.resolve, nextFn.reject, nextFn.attempts)
     }
-    this.promise = null;
-    if (typeof this.callback === 'function') this.callback();
-    return true;
+    this.promise = null
+    if (typeof this.callback === 'function') this.callback()
+    return true
   }
 
-  _wrap(fn, resolve, reject, attempts) {
-    let retryCount = 0;
+  _wrap (fn, resolve, reject, attempts) {
+    let retryCount = 0
     const retry = (err) => {
       if (retryCount >= attempts) {
-        throw err || new Error('Unknown Error');
+        throw err || new Error('Unknown Error')
       }
-      retryCount += 1;
-      return this._promised(fn).catch(retry);
-    };
+      retryCount += 1
+      return this._promised(fn).catch(retry)
+    }
     return retry()
-      .then((r) => { resolve(r); }, (e) => { reject(e); })
-      .then(() => this._next());
+      .then((r) => { resolve(r) }, (e) => { reject(e) })
+      .then(() => this._next())
   }
-
 }
 
-module.exports = PromiseQueue;
+module.exports = PromiseQueue
